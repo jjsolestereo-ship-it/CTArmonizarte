d5b6b726ba04167345fcff3faee1e491c1235b77
Tu Nombre <tu-correo@ejemplo.com>
2026-03-10 17:41:21 -0500

    primer commit proyecto CTA

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API CTArmonizarte funcionando...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
